f4b78f755a93d4a92ccfca64293af11f80bb715c
Nick Kuropatkin <nikolay.kuropatkin@gentechpartners.com>
2015-01-11 12:17:22 +0200

    add nginx conf,index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    define('RDS','php-redis-0.9/lib/');
+    require_once RDS.'redis.php';
+    require_once RDS.'redis.pool.php';
+    require_once RDS.'redis.peer.php';
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Redis</title>
+</head>
+<body>
+<div class="header"></div>
+<div class="body">
+    <?='its works';?>
+</div>
+<div class="footer"></div>
+</body>
+</html>
